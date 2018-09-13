b1fe9baf186ea9afa3fb5b1f1fec6abcefaee7cf
Laura Felce <crepu83@yahoo.it>
2018-09-13 08:32:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,4 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist;
 }
 
-function removeFromPlaylist(playlist, artistName
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Slowdive;
+  return playlist;
+}

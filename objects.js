a0eebb86909161086cf6e869167292d668897da5
Laura Felce <crepu83@yahoo.it>
2018-09-13 08:10:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,8 @@
 var playlist = {Albano: "Felicita"};
 
 
-playlist['Slowdive'] = 'Alison';
-playlist['My Bloody Valentine'] = 'Sometimes';
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[Arisa] = 'Noia';
+  
   
   return playlist;
 }

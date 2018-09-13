04dd1e9b55a2e17058f77316fc140a140170c9c6
Laura Felce <crepu83@yahoo.it>
2018-09-13 08:23:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist = {Albano: "Felicita"};
 var playlist = {Slowdive: "Alison"};
 var playlist = {MyBloodyValentine: "Alison"};
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.PhilOchs = 'Heres to the State of Mississippi';
+  playlist.['Phil Ochs'] = 'Heres to the State of Mississippi';
   return playlist;
 }
